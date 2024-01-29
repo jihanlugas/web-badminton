b373254b2c45e4158544515d3dd3a6bba0ef983f
jihanlugas <jihanlugas2@gmail.com>
2024-01-30 00:33:20 +0700

    perbaikan sign in dan modifikasi checkbox

--- src/components/formik/checkbox-field.tsx
+++ src/components/formik/checkbox-field.tsx
@@ -8,12 +8,12 @@ interface Props extends React.HTMLProps<HTMLInputElement> {
 
 const CheckboxField: NextPage<Props> = ({ name, showError = true, ...props }) => {
 	return (
-		<div className={'flex flex-col w-full'}>
+		<div className={'flex flex-col w-full pl-1'}>
 			<div className='flex items-center'>
 				{props.label && (
 					<label className={'select-none w-full py-2 flex items-center'} >
 						<FastField
-							className={'mr-2 accent-primary-500 py-2 h-5 w-5'}
+							className={'mr-2 accent-gray-700 py-2 scale-150'}
 							type={'checkbox'}
 							name={name}
 							{...props}
